189a2fce452aeed78ba10731d6f5812c3d3e8dda
codernik14 <43906771+codernik14@users.noreply.github.com>
2021-01-25 16:46:10 +0530

    Delete sites.sql

--- Sql_files/sites.sql
+++
@@ -1,3 +0,0 @@
-insert into Sites values (1,'10.3.5.215','iiit123','user','QuarantinedAgain','mysql');
-insert into Sites values (2,'10.3.5.214','iiit123','user','QuarantinedAgain','mysql');
-insert into Sites values (3,'10.3.5.213','iiit123','user','QuarantinedAgain','mysql');
