4ae04eb8b7e12c287e45c6459af052c6869b2fce
codernik14 <43906771+codernik14@users.noreply.github.com>
2021-01-25 16:45:50 +0530

    Delete column_info.sql

--- Sql_files/column_info.sql
+++
@@ -1,9 +0,0 @@
--- column_name, NULL_col , Extras, Default_col, Data_type_col, Key_col, Table_id
-insert into column_info values ('DOB', 'NO', '', 'NULL', 'DATE', 'NONE', 1);
-insert into column_info values ('DOJ', 'NO', '', 'NULL', 'DATE', 'NONE', 1);
-
-INSERT INTO column_info VALUES ('Course_Code','NO', '', 'NULL','INT', 'PRI',2);
-INSERT INTO column_info VALUES ('Course_Name','NO', '', 'NULL','VARCHAR(100)', 'UNI',2);
-
-INSERT INTO column_info VALUES ( 'Faculty_Id', 'NO', '', 'NULL','INT', 'PRI',4);
-INSERT INTO column_info VALUES ( 'First_Name', 'NO', '', 'NULL','VARCHAR(100)', 'NONE',4);
