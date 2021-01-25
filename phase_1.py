f83c210b1c9557a5b13aa70dcd0c5911b3806083
Maxslide <manaskabre99@gmail.com>
2021-01-25 22:47:06 +0530

    Added Code

--- phase_1.py
+++ phase_1.py
@@ -9,13 +9,24 @@ class QuarantinedAgain():
     def Get_Fragments(self):
         query = "SELECT * FROM Frag_Table;" 
         self.cursor.execute(query)
+        print("id Fragment_name Condition")
         for (i,j,k,l) in self.cursor:
-            print(i,j)
+            print(i,j,k)
     
     def Get_Allocation(self,frag_id):
         query = "Select * FROM Allocation Where Frag_Id = " + str(frag_id) + ";"
         self.cursor.execute(query)
         for i in self.cursor:
             # print(i)
-            print("Frag_Id = " + i[0] + " allocated on site : " +i[1])
+            print("Frag_Id = " + str(i[0]) + " allocated on site : " +str(i[1]))
+
+
+obj = QuarantinedAgain("Maxslide", "iiit123","127.0.0.1","QuarantinedAgain")
+obj.Get_Fragments()
+
+print("Enter Fragment id for which the Allocation schema is required : ")
+
+frag_id = input()
+
+obj.Get_Allocation(frag_id)
 
