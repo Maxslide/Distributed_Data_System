58bca8b81a30d1fbc9d0a71b91c048db0a096979
Maxslide <manaskabre99@gmail.com>
2021-02-24 15:46:18 +0530

    main

--- phase_2/main.py
+++ phase_2/main.py
@@ -1,6 +1,6 @@
 import sqlparse
 
-query = "SELECT A,B,C FROM t1,t2,t3 WHERE t1.id == t2.id and t2.name = t3.name and (t1.col > 5 or t3.city == 'Bangalore')"
+query = "SELECT A,B,C FROM t1,t2,t3 WHERE t1.id == t2.id and t2.name == t3.name and (t1.col > 5 or t3.city == 'Bangalore')"
 
 # conevrt this into query tree
 
@@ -19,6 +19,14 @@ for i in parsed_query:
 
 for i in token_list:
     print(i)
+    # if(type(i) == list):
+    #     for j in i:
+    #         print(j)
+
+
+#  Where -> Joins, Condition
+# [Select, From, Join, Join]
+# [t1,t2,t3, join(t1,t2,condition),join(t3,codition), Condition(table_name,condition), Condition(table_name,condition)...., SELECT(Columns)]
 
 tree_nodes = []
 for i in range(len(token_list)):
